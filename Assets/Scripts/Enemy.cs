1850f232ab442d83c570518e25a63c0ee7c552b7
suminp96 <97658764+Luci-Park@users.noreply.github.com>
2022-08-22 07:55:31 +0900

    error tweaked

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -160,15 +160,11 @@ public class Enemy : MonoBehaviour
 
     }
 
-        private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            if (timeBetweenAttack < 0)
-            {
-                timeBetweenAttack = startTimeBetweenAttack;
-                collision.gameObject.GetComponent<PlayerHealthSystem>().TakeDamage(damage);
-            }
+            collision.gameObject.GetComponent<PlayerHealthSystem>().TakeDamage(damage);
         }
 
 
--- Assets/Scripts/PlayerHealthSystem.cs
+++ Assets/Scripts/PlayerHealthSystem.cs
@@ -20,27 +20,51 @@ public class PlayerHealthSystem : MonoBehaviour
     private Animator anim;
 
     int maxHeart = 7;
+    float invincibleTime;
+    const float maxInvincibleTime = 1f;
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
-
+        invincibleTime = maxInvincibleTime;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        health = maxHeart;
-        if (health > maxHeart)
-        {
-            health = maxHeart;
-        }
+        invincibleTime -= Time.deltaTime;
+    }
 
+    public void GiveHealth(int health)
+    {
+        this.health += health; 
+        SetHearts();
+        if (this.health > maxHeart)
+        {
+            this.health = maxHeart;
+        }
+    }
+
+   public void TakeDamage(int damage)
+    {
+        if (invincibleTime > 0) return;
+        invincibleTime = maxInvincibleTime;
+        print(damage);
+        shake.ShakeStart(0.2f, 1, 1f);
+        anim.SetTrigger("Hurt");
+        Instantiate(HurtParticle, transform.position, Quaternion.identity);
+        health -= damage;
+        SetHearts();
         if (health <= 0)
         {
             Destroy(gameObject);
             LoseScreen.SetActive(true);
         }
+    }
+
+    void SetHearts()
+    {
+        Debug.Log("sethearts");
         for (int i = 0; i < hearts.Length; i++)
         {
             if (i < health)
@@ -51,30 +75,6 @@ public class PlayerHealthSystem : MonoBehaviour
             {
                 hearts[i].sprite = emptyHeart;
             }
-
-
-            if (i < numOfHearts)
-            {
-                hearts[i].enabled = true;
-            }
-            else
-            {
-                hearts[i].enabled = false;
-            }
         }
-    }
-
-    public void GiveHealth(int health)
-    {
-        this.health += health;
-    }
-
-   public void TakeDamage(int damage)
-    {
-        print(damage);
-//        shake.ShakeStart(0.2f, 1, 1f);
-        anim.SetTrigger("Hurt");
-        Instantiate(HurtParticle, transform.position, Quaternion.identity);
-        health -= damage;
     }
 }
